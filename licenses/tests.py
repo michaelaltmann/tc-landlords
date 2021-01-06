ac3642ec2a33b082410e9c7d5540bd444e2fc162
maltmann <michael.altmann@gmail.com>
2021-01-06 10:42:15 -0600

    Add tests for cleanName

--- licenses/tests.py
+++ licenses/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from transform import cleanName
 
-# Create your tests here.
+import unittest
+
+
+class TestCleanName(unittest.TestCase):
+    def testLower(self):
+        self.assertEqual(cleanName('This is A test'), 'this is a test')
+
+    def testLLC(self):
+        self.assertEqual(cleanName('cat llLLC'), 'cat llllc')
+        self.assertEqual(cleanName('Llama LLC'), 'llama')
+        self.assertEqual(cleanName('Frog, LC'), 'frog')
+        self.assertEqual(cleanName('LLC Baboon, LlC'), 'llc baboon')
+        self.assertEqual(cleanName('229 Queen Rental LLC'), '229 queen rental')
+
+    def testSpaces(self):
+        self.assertEqual(cleanName('   a  b  c  '), 'a b c')
+
+
+if __name__ == '__main__':
+    unittest.main()
