5ac353df81105565257a5321c2893753e5a36cdd
maltmann <michael.altmann@gmail.com>
2021-12-05 11:20:19 -0600

    Remove Azure and Tableau libs

--- util/adls.py
+++ util/adls.py
@@ -1,4 +1,3 @@
-from azure.storage.filedatalake import DataLakeServiceClient
 import os
 from pathlib import Path
 import urllib.request
@@ -7,25 +6,6 @@ import urllib.request
 Manage access to ADLS data storage
 '''
 
-def download_using_adls_file(file_name):
-  STORAGE_ACCNT='tclandlords'
-  CONTAINER='tc-landlords-data'
-  remote_directory='data/gen'
-  local_directory = 'data/gen'
-  storage_account_key = os.environ.get('STORAGE_ACCOUNT_KEY')
-  service = DataLakeServiceClient(account_url=f"https://{STORAGE_ACCNT}.dfs.core.windows.net", credential=storage_account_key)
-  file_system_client = service.get_file_system_client(CONTAINER)
-  file_path= f'{local_directory}/{file_name}'
-  directory_client = file_system_client.get_directory_client(remote_directory)
-  file_client = directory_client.get_file_client(file_name)
-  Path(local_directory).mkdir(parents=True, exist_ok=True)
-  local_file = open(file_path,'wb')
-
-  download = file_client.download_file()
-  downloaded_bytes = download.readall()
-  print("Downloaded", len(downloaded_bytes), f"bytes from {CONTAINER} {remote_directory}/{file_name}")
-  local_file.write(downloaded_bytes)
-  local_file.close()
 
 def download_file(file_name):
   # https://tclandlords.blob.core.windows.net/tc-landlords-data/data/gen/tags.csv
